95eed89b199147f5ec9ccb35169ff1c24c8311ad
Chutimasuk01 <chutima18092544@gmail.com>
2023-11-22 00:18:34 +0700

    .commit

--- Myapp1.py
+++ Myapp1.py
@@ -6,6 +6,14 @@ import streamlit as st
 st.header("การวิเคราะห์คุณสมบัติการขอสินเชื่อบ้าน")
 st.subheader("ชุติมา สุขสมัย  \t สาขาวิทยาการข้อมูล \t NPRU")
 
+col1, col2 =st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('pic/Npru.jpg')
+with col2:
+    st.image('pic/002.jpg')
+    
 st.image('pic/Home2.jpg')
 
     
--- pages/3.Predict.py
+++ pages/3.Predict.py
@@ -9,6 +9,8 @@ col1, col2 =st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
+    st.image('pic/002.jpg')
+with col2:
     st.image('pic/Home1.jpg')
 
 
