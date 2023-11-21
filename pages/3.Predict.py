8193893c08a4bea6e1fda3771495bea5a2e54a65
Chutimasuk01 <chutima18092544@gmail.com>
2023-11-21 15:09:19 +0700

    Update 3.Predict.py

--- pages/3.Predict.py
+++ pages/3.Predict.py
@@ -101,7 +101,7 @@ if st.button("ทำนายผล"):
    if out[0]=="0":
       #st.image("./pic/iris.jpg")
       st.header("No")
-   elif out[1]=="0":
+   elif out[0]=="1":
       #st.image("./pic/iris3.jpg")  
       st.header("Yes")
    st.button("ไม่ทำนาย")
